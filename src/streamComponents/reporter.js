33cb51d2713af7e959c60228a64d913505d5f6dc
unknown <mehradhetfield@gmail.com>
2026-05-09 20:43:33 +0330

    feat: progress reporter implementation

--- src/streamComponents/reporter.js
+++ src/streamComponents/reporter.js
@@ -1,14 +1,30 @@
 import { PassThrough } from "node:stream";
 import { log } from "../util";
 
+const A_HUNDRED_PERCENT = 100;
 export default class Reporter {
   #loggerFn;
   constructor({ logger = log } = {}) {
     this.#loggerFn = logger;
   }
 
+  #onData(amount) {
+    let totalChunks = 0;
+    return (chunk) => {
+      totalChunks += chunk.length;
+      const processed = (A_HUNDRED_PERCENT / amount) * totalChunks;
+      console.log("from reporter", amount);
+
+      this.#loggerFn(`processed ${processed.toFixed(2)}%`);
+    };
+  }
+
   progress(amount) {
     const progress = PassThrough();
+    progress.on("data", this.#onData(amount));
+    progress.on("end", () =>
+      this.#loggerFn(`processed ${A_HUNDRED_PERCENT}.00%`),
+    );
     return progress;
   }
 }
