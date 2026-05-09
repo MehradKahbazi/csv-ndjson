82e0927c046a2e95351f4602f63bd87b5f2d776d
unknown <mehradhetfield@gmail.com>
2026-05-09 18:04:08 +0330

    chore: test todo listing

---
+++ test/streamComponents/scvtondjson.test.js
@@ -0,0 +1,8 @@
+import { expect, describe, it } from "@jest/globals";
+import CSVToNDJSON from "../../src/streamComponents/csvtondjson.js";
+
+describe('CSV to NDJSON test suit', () =>{
+    it.todo('give a csv string it should return a ndjson string')
+    it.todo('it should work with strings that do not have breakline at the end')
+    it.todo('it should work with files that have breaklines in the begining of string')
+})
