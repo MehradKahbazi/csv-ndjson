5e9b680ee2871b1e546606da1a26b5e5d04dad8b
unknown <mehradhetfield@gmail.com>
2026-05-09 18:51:26 +0330

    chore: converting csv string to json string

--- src/streamComponents/csvtondjson.js
+++ src/streamComponents/csvtondjson.js
@@ -1,7 +1,12 @@
 import { Transform } from "node:stream";
+
+const BREAK_LINE_SYMBOL = "\n";
+const INDEX_NOT_FOUND = -1;
 export default class CSVToNDJSON extends Transform {
   #delimiter = "";
   #headers = [];
+  #buffer = Buffer.alloc(0);
+
   constructor({ delimiter = ",", headers }) {
     super();
 
@@ -9,8 +14,49 @@ export default class CSVToNDJSON extends Transform {
     this.#headers = headers;
   }
 
+  *#updateBuffer(chunk) {
+    this.#buffer = Buffer.concat([this.#buffer, chunk]);
+    let breaklineIndex = 0;
+
+    while (breaklineIndex !== INDEX_NOT_FOUND) {
+      breaklineIndex = this.#buffer.indexOf(Buffer.from(BREAK_LINE_SYMBOL));
+      if (breaklineIndex === INDEX_NOT_FOUND) break;
+
+      const lineToProcessIndex = breaklineIndex + BREAK_LINE_SYMBOL.length;
+      const line = this.#buffer.subarray(0, lineToProcessIndex);
+      const lineData = line.toString();
+      console.log(lineData);
+
+      // remove from the main buffer data
+      this.#buffer = this.#buffer.subarray(lineToProcessIndex);
+
+      // if it's an empty line ignore it
+      if (lineData === BREAK_LINE_SYMBOL) continue;
+
+      const NDJSONLine = [];
+      const headers = Array.from(this.#headers);
+      for (const item of lineData.split(this.#delimiter)) {
+        const key = headers.shift();
+        const value = item.replace(BREAK_LINE_SYMBOL, "");
+
+        if (key === value) break;
+        console.log({ key, value });
+
+        NDJSONLine.push(`"${key}":"${value}"`);
+      }
+      if (!NDJSONLine.length) continue;
+      const NDJSONData = NDJSONLine.join(",");
+      yield Buffer.from(
+        "{".concat(NDJSONData).concat("}").concat(BREAK_LINE_SYMBOL),
+      );
+    }
+  }
+
   _transform(chunk, encoding, callback) {
-    callback(null, chunk);
+    for (const item of this.#updateBuffer(chunk)) {
+      this.push(item);
+    }
+    callback();
   }
 
   _final(callback) {
