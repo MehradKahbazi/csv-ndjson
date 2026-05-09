a6cdda1464e1a89af133dca947abe5d3eb64d0f6
unknown <mehradhetfield@gmail.com>
2026-05-09 18:18:31 +0330

    test: first test

--- test/streamComponents/scvtondjson.test.js
+++ test/streamComponents/scvtondjson.test.js
@@ -1,8 +1,30 @@
-import { expect, describe, it } from "@jest/globals";
+import { expect, describe, it, jest } from "@jest/globals";
 import CSVToNDJSON from "../../src/streamComponents/csvtondjson.js";
 
-describe('CSV to NDJSON test suit', () =>{
-    it.todo('give a csv string it should return a ndjson string')
-    it.todo('it should work with strings that do not have breakline at the end')
-    it.todo('it should work with files that have breaklines in the begining of string')
-})
+describe("CSV to NDJSON test suit", () => {
+  it("give a csv string it should return a ndjson string", () => {
+    const csvString = `id,name,address\n01,Mehrad,address01\n`;
+    const csvToJSON = new CSVToNDJSON({
+      delimiter: ",",
+      headers: ["id", "name", "address"],
+    });
+
+    const expected = JSON.stringify({
+      id: "01",
+      name: "Mehrad",
+      address: "address01",
+    });
+    
+    const fn = jest.fn();
+    csvToJSON.on("data", fn);
+    csvToJSON.write(csvString);
+    csvToJSON.end();
+
+    const [current] = fn.mock.lastCall;
+    expect(current).toStrictEqual(expected);
+  });
+  it.todo("it should work with strings that do not have breakline at the end");
+  it.todo(
+    "it should work with files that have breaklines in the begining of string",
+  );
+});
