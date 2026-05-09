8827da397c7af9e8e67bef95b923a1e38f61811f
unknown <mehradhetfield@gmail.com>
2026-05-09 20:24:43 +0330

    chore: reporter test suit

---
+++ test/streamComponents/reporter.test.js
@@ -0,0 +1,25 @@
+import { expect, describe, it, jest, afterAll } from "@jest/globals";
+import Reporter from "../../src/streamComponents/reporter.js";
+
+describe("Reporter Test Suit", () => {
+  it("should print progress status correctly", () => {
+    const loggerMock = jest.fn();
+    const reporter = new Reporter({
+      logger: loggerMock,
+    });
+
+    const multiple = 10;
+    const progress = reporter.progress();
+    for (let i = 1; i < multiple; i++) {
+      progress.write("1");
+    }
+    progress.emit("end");
+    expect(loggerMock.mock.calls.length).toEqual(10);
+
+    for (const index in loggerMock.mock.calls) {
+      const [call] = loggerMock.mock.calls[index];
+      const expected = (Number(index) + 1) * multiple;
+      expect(call).toStrictEqual(`processed ${expected}.00%`);
+    }
+  });
+});
